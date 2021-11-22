0e18f39223fddb98c61926ca9c01a7eeab41f17c
Nicolas Reyland <nicolas@reyland.dev>
2021-11-22 16:15:10 +0100

    added empty language src files

---
+++ C/interpret.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: interpret input-file\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    //
+}
